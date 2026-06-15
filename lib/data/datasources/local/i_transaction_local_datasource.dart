8165e166aab2ec509aea7abb7582d3fecb49ac28
adcerro <adcerro@uninorte.edu.co>
2026-06-15 16:24:11 -0500

    renamed_movement to transaction

--- lib/data/datasources/local/i_transaction_local_datasource.dart
+++ lib/data/datasources/local/i_transaction_local_datasource.dart
@@ -1,11 +1,11 @@
 import 'package:tester/domain/entities/transaction.dart';
 
 abstract class IMoneyMovementLocalDatasource {
-  Future<List<Transaction>> getUserMovements({required String user});
+  Future<List<Transaction>> getUserTransaction({required String user});
 
-  Future<void> addMovement({required Transaction movement});
+  Future<void> addTransaction({required Transaction transaction});
 
-  Future<void> updateMovement({required Transaction movement});
+  Future<void> updateTransaction({required Transaction transaction});
 
-  Future<void> deleteMovement({required double id});
+  Future<void> deleteTransaction({required double id});
 }
